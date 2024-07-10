ea3af0ad31576dfbe92cf6926474caff2cd20868
Prince <prince_22410@aitpune.edu.in>
2024-07-10 15:20:12 +0530

    pricing

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ $(document).ready(function() {
             $(".logo").removeClass("logo").addClass("logo_as");
             $(".links").removeClass("links").addClass("links_as");
             $(".bt").removeClass("bt").addClass("bt_as");
+            $("body").css("background-color", "#002124");
         } else {
             $(".navbar_as").removeClass("navbar_as").addClass("navbar_bs");
             $(".logo_as").removeClass("logo_as").addClass("logo");
             $(".links_as").removeClass("links_as").addClass("links");
             $(".bt_as").removeClass("bt_as").addClass("bt");
+            $("body").css("background-color", " #001d21");
         }
         $('section').each(function() {
             var sectionTop = $(this).offset().top;
