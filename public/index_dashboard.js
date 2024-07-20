cfe4bf90a386365075b48c01aa2e69e6ec37a965
Prince <prince_22410@aitpune.edu.in>
2024-07-20 23:53:08 +0530

    modal for add transaction

--- public/index_dashboard.js
+++ public/index_dashboard.js
@@ -62,31 +62,27 @@ $(document).ready(function() {
     //     }
     // });
 
-    var modal = document.getElementById("myModal");
+    $(document).ready(function() {
+        var modal = $("#myModal");
+        var btn = $(".open");
+        var span = $(".close");
+    
+        btn.click(function() {
+            modal.show();
+        });
+        span.click(function() {
+            modal.hide();
+        });
+        $(window).click(function(event) {
+            if ($(event.target).is(modal)) {
+                modal.hide();
+            }
+        });
+    });
+    
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("openModal");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-
+    function resetForm(formId) {
+        document.getElementById(formId).reset();}
 
   $('.card').each(function() {
       const img = $(this).find('.card-img');
